6d9f10821afcb4fb4a51b927c9035bac5a37b2af
betterscrib <betterscrib@gmail.com>
2021-09-13 12:16:42 +0200

    length password change in models

--- fapp/models.py
+++ fapp/models.py
@@ -12,4 +12,4 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(256))
